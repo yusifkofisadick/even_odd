c7233b686548ba598663b72a3ec9d4039fe42506
yusif kofi sadick <124945382+yusifkofisadick@users.noreply.github.com>
2024-11-09 16:29:29 -0800

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,8 @@
+print("       Even/odd number identifier")
+
+
+num = int(input("Enter an integer here.......?"))
+
+if num % 2 == 0:
+    print(num, "is an even number")
+else: print(num, "is an odd number")
